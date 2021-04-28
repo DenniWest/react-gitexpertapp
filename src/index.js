98f0ad43c58803f35d4517d74bdf3204b5a9b07c
Dennison Torres <dennisontorres@gmail.com>
2021-04-27 22:14:28 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GifExpertApp } from './GifExpertApp';
+
+import './index.css';
+
+
+
+ReactDOM.render(
+ 
+    <GifExpertApp />,
+  
+  document.getElementById('root')
+);
+
+
+// Tarea 
+// 1. crear componente GifExpertApp
+// 2. <h2>GifExpertApp</h2>
+// 3. <hr />
+//zAFs6uNY5CIeTtaFRKHvJoAmaYEP1PSL
